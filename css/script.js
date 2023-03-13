898554d0f4212bd1bae740987683c42c7420c219
raza <raza1010abbas@gmail.com>
2023-03-14 02:43:30 +0530

    first

---
+++ css/script.js
@@ -0,0 +1,8 @@
+console.log("script running....")
+document.querySelector('.cross').style.display='none';
+document.querySelector('.burger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo')
+  
+});
+
+  
